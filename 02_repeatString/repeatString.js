b1d4961a264e7aaa7ff3469361fe3068e0d73e40
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-05-20 16:38:21 -0700

    finished 1st test exercise, started 2nd
    this is the first way i am writing the 2nd test exercise to get an idea of how to think about it. it has an error so i need to work on it some more

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let string = "";
+    while (times > 0) {
+        repeatString += string;
+        times--;
+    }
+    return repeatString;
 };
 
 // Do not edit below this line
