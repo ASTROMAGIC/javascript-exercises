e44fb705eedaa7ec2ad38f4e91c27e12a0e00c7f
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-05-20 17:15:09 -0700

    5 of 7 tests passed for exercise 2
    and i was able to come up with the solution by searching for the methods myself. i'm proud of that

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,15 @@
 const repeatString = function(string, times) {
     if (times > 0)
         return string.repeat(times);
+    
+    else if (times <0) {
+        return "ERROR";
+    }
+
     else 
         return "";
+
+    
 };
 
 // Do not edit below this line
