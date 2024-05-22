785f62203f7a95819f1c4a0e5468e93adf984864
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-05-22 14:50:28 -0700

    finished sum function tests
    this one wasn't too bad, but i need to take the time to understand these better. Like, I think I should be taking this much more slowly and shouldn't do more than 2 of these exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (typeof a !== 'number' || typeof b !== 'number') return 'ERROR';
+    if (isNaN(a) || isNaN(b)) return 'ERROR';
+    if (a < 0 || b <0) return 'ERROR';
+    let sum = 0;
+    for (let i =Math.min(a,b); i <= Math.max(a,b); i++) {
+        sum += i;
+    }
 
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+    // this is to ensure that the minimum value and maximum value go into the correct places
+    return sum;
 };
 
 // Do not edit below this line
