5cd815fe1a10cb64a3da5436e9d9e51e113715f3
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-06-03 10:09:06 -0700

    completed leapYears function
    wrote a basic function that will determine if a given year input is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(a) {
+if (a % 400 === 0) {
+    return true;
+}   else if (a % 100 === 0) {
+    return false; 
+}   else if (a % 4 === 0) {
+    return true;
+}   else {
+    return false; 
+}
+
+// I should look for ways to write my code more concisely as time goes on. But for now, this works. I should write my function parameters to be specific inputs instead of generic letters. I need to be aware of syntax structure in functions and in general for javascript. This just requires more consistent practice. 
 
 };
 
